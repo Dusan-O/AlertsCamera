a831b7a4d6cc470075321b9eeca8dd034fe14e5c
Dusan-O <dusan.orescanin@gmail.com>
2022-08-17 01:38:41 +0200

    Preparation of the Camera

--- AlertsCamera/ViewController.swift
+++ AlertsCamera/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
 
@@ -13,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var containerIV: UIImageView!
     @IBOutlet weak var editingButton: UIButton!
     
+    var libraryPicker: PHPickerViewController?
+    var cameraPicker = UIImagePickerController()
+    var canUserCamera = UIImagePickerController.isSourceTypeAvailable(.camera)
+    var canEdit = false
     
     
     override func viewDidLoad() {
@@ -29,3 +34,51 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: PHPickerViewControllerDelegate {
+    
+    func setupLibrary() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 1
+        configuration.preferredAssetRepresentationMode = .automatic
+        libraryPicker = PHPickerViewController(configuration: configuration)
+        libraryPicker?.delegate = self
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let first = results.first else { return }
+        let newPhotoProvider = first.itemProvider
+        guard newPhotoProvider.canLoadObject(ofClass: UIImage.self) else { return }
+        newPhotoProvider.loadObject(ofClass: UIImage.self) { image, error in
+            DispatchQueue.main.async {
+                if let e = error {
+                    print(e.localizedDescription)
+                }
+                guard let newImage = image as? UIImage else { return }
+                self.containerIV.image = newImage
+            }
+        }
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func setupCamera() {
+        if canUserCamera {
+            cameraPicker.delegate = self
+            cameraPicker.sourceType = .camera
+            cameraPicker.allowsEditing = canEdit
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let infoKey: UIImagePickerController.InfoKey = canEdit ? .editedImage : .originalImage
+        guard let image = info[infoKey] as? UIImage else { return }
+        self.containerIV.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
